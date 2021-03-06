15f0987cc67e52df6838971d5aa609493560bed2
Mahesh <53971467+maheshwaranvk@users.noreply.github.com>
2021-03-06 22:29:06 +0530

    BootCamp

---
+++ src/test/java/week1/day1/SA25CreateIndividuals.java
@@ -0,0 +1,65 @@
+package week1.day1;
+//https://mercury-bootcamp.atlassian.net/browse/SAL-25?atlOrigin=eyJpIjoiZjg1NmQ2YWZmYjBkNDM1MGE4OTY4ZGVlNjMwZDU0ZjMiLCJwIjoiaiJ9
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SA25CreateIndividuals {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		RemoteWebDriver driver = new ChromeDriver(options);
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		driver.get("https://login.salesforce.com");
+		driver.manage().window().maximize();
+		
+		driver.findElementById("username").sendKeys("mercury.bootcamp@testleaf.com");
+		driver.findElementById("password").sendKeys("Bootcamp$123");
+		driver.findElementById("Login").click();
+		
+		driver.findElementByXPath("//div[@class='slds-icon-waffle']").click();
+		driver.findElementByXPath("//button[text()='View All']").click();
+		
+		WebElement ind = driver.findElementByXPath("//p[text()='Individuals']");
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click()", ind);
+		
+		driver.findElementByXPath("//div[@title='New']").click();
+		
+		driver.findElementByXPath("//span[@class='slds-truncate' and text()='Individuals']/following::*[@class='slds-icon slds-icon-text-default slds-icon_xx-small' and @data-key='chevrondown']").click();
+		
+		
+		
+		js.executeScript("arguments[0].click()", driver.findElementByXPath("//span[@one-appnavbarmenuitem_appnavbarmenuitem='' and text()='New Individual']"));
+		String lName = "Mahesh Kumar";
+		driver.findElementByXPath("//input[@placeholder='Last Name']").sendKeys(lName);
+		driver.findElementByXPath("//button[@class='slds-button slds-button--neutral uiButton--brand uiButton forceActionButton']").click();
+		
+		String name = driver.findElementByXPath("//div[@class='slds-page-header__title slds-m-right--small slds-align-middle clip-text slds-line-clamp']/span").getText();		
+		
+		if(lName.equalsIgnoreCase(name))
+		{
+			System.out.println("Individuals Created");
+		}
+		else
+		{
+			System.out.println("Individuals not Created");
+		}
+			
+	}
+}
---
+++ src/test/java/week1/day1/SAL26EditIndividuals.java
@@ -0,0 +1,68 @@
+package week1.day1;
+//https://mercury-bootcamp.atlassian.net/browse/SAL-26
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SAL26EditIndividuals {
+
+	public static void main(String[] args) throws InterruptedException {
+WebDriverManager.chromedriver().setup();
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		RemoteWebDriver driver = new ChromeDriver(options);
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		driver.get("https://login.salesforce.com");
+		driver.manage().window().maximize();
+		driver.findElementById("username").sendKeys("mercury.bootcamp@testleaf.com");
+		driver.findElementById("password").sendKeys("Bootcamp$123");
+		driver.findElementById("Login").click();
+		
+		driver.findElementByXPath("//div[@class='slds-icon-waffle']").click();
+		driver.findElementByXPath("//button[text()='View All']").click();
+		
+		WebElement ind = driver.findElementByXPath("//p[text()='Individuals']");
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click()", ind);
+		
+		
+		js.executeScript("arguments[0].value='kumar'", driver.findElementByXPath("//input[@name='Individual-search-input']"));
+		js.executeScript("arguments[0].click()", driver.findElementByXPath("(//div[@class='slds-grid'])[1]"));
+	
+		js.executeScript("arguments[0].click()", driver.findElementByXPath("//button[@name='refreshButton']"));
+		js.executeScript("arguments[0].click()", driver.findElementByXPath("(//lightning-icon[@class='slds-icon-utility-down slds-icon_container']/lightning-primitive-icon)[2]"));
+		js.executeScript("arguments[0].click()", driver.findElementByXPath("//a[@role='menuitem' and @title='Edit']"));
+		  
+//		  driver.findElementByXPath("(//lightning-icon[@class='slds-icon-utility-down slds-icon_container']/lightning-primitive-icon)[2]").click();
+		  
+		  driver.
+		  findElementByXPath("//div[@class='salutation compoundTLRadius compoundTRRadius compoundBorderBottom form-element__row uiMenu']//a"
+		  ).click(); driver.
+		  findElementByXPath("//li[@class='uiMenuItem uiRadioMenuItem']/a[@title='Mr.']"
+		  ).click(); driver.
+		  findElementByXPath("//input[@class='firstName compoundBorderBottom form-element__row input']"
+		  ).sendKeys("Ganesh"); driver.
+		  findElementByXPath("//button[@class='slds-button slds-button--neutral uiButton--brand uiButton forceActionButton']"
+		  ).click();
+		  
+		  String text = driver.
+		  findElementByXPath("//span[@class='slds-grid slds-grid--align-spread forceInlineEditCell']/a"
+		  ).getText(); if(text.contains("Ganesh")) {
+		  System.out.println("FirstName Updated"); } else {
+		  System.out.println("Not Updated"); }
+		 
+		 
+	}
+}
