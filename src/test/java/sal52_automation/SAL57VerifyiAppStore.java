7b9f345495440e585e2b064eea631bdc84b08c15
Mahesh <53971467+maheshwaranvk@users.noreply.github.com>
2021-04-04 12:57:56 +0530

    0404

--- src/test/java/sal52_automation/SAL57VerifyiAppStore.java
+++ src/test/java/sal52_automation/SAL57VerifyiAppStore.java
@@ -14,19 +14,14 @@ public class SAL57VerifyiAppStore extends ProjectSpecificMethods{
 	public void verifyiApp() {
 		
 		String url ="https://apps.apple.com/us/app/salesforcea/";
-		
-		
 		driver.findElementById("tab_setupHomeCarousel_page3").click();
 		driver.findElementByXPath("//span[text()='App Store']").click();
 		
 		Set<String> windowHandles = driver.getWindowHandles();
 		List<String>windowHandlesList = new ArrayList<String>(windowHandles);
-		
 		driver.switchTo().window(windowHandlesList.get(1));
 		
 		driver.findElementByXPath("//button[text()='Confirm']").click();
-		
-		
 		as.assertTrue(driver.getCurrentUrl().contains(url));
 		as.assertAll();
 		
